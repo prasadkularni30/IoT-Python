588a977f598f47ea8dd57d4651c0e092c024b766
prasadkularni30 <kulkarniprasad3001@gmail.com>
2024-06-27 19:59:26 +0530

    variable declaration

--- var_declare.py
+++ var_declare.py
@@ -3,7 +3,14 @@ b="prasad"
 c='a'
 d=4
 e=5.6
+status= True
 print("string:",a,b)
+print(type(a))
 print("char:",c)
+print(type(c))
 print("int:",d)
+print(type(d))
 print("float:",e)
+print(type(e))
+print("Bool: ",status)
+print(type(status))
