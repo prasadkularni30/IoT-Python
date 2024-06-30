dad881a29d19a02063a8c63f2dabd83fdda13314
prasadkularni30 <kulkarniprasad3001@gmail.com>
2024-06-30 22:02:24 +0530

    prac

---
+++ tuple.py
@@ -0,0 +1,19 @@
+
+#tuple={"prasad","shubham"}
+#immutable
+"""
+tuple=input({})
+print(tuple)
+"""
+
+tuplee=(
+    "prasad",
+    "shubham",
+    34
+    )
+print(tuplee)
+print(type(tuplee))
+print(tuplee[0])
+print(tuplee[-1])
+# tuplee[1]=55
+print(tuplee)
